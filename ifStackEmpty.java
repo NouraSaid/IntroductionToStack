49c816c1dddb633ec79a7f0ac7e4d41c1b40a5d3
Noura Said AL Saidi <97401813+NouraSaid@users.noreply.github.com>
2024-02-21 12:35:44 +0400

    Add files via upload

---
+++ ifStackEmpty.java
@@ -0,0 +1,10 @@
+import java.util.Stack;
+
+public class FirstQuestions {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        System.out.println(stack.empty());
+
+    }
+}
