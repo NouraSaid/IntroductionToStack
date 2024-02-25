7577b49435fa5e657a72d55192595370ce0e204b
Noura Said AL Saidi <97401813+NouraSaid@users.noreply.github.com>
2024-02-25 09:21:10 +0400

    Update FirstQuestions.java

--- FirstQuestions.java
+++ FirstQuestions.java
@@ -4,18 +4,8 @@ public class FirstQuestions {
 
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
-        System.out.println(stack.empty());
+        System.out.println(stack.size(10));
 
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.push(5);
-        stack.push(6);
-        stack.push(7);
-        stack.push(8);
-        stack.push(9);
-        stack.push(10);
 
 
     }
